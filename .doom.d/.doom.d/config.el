08ea429a864efa0a38c32cf59939b3fc0e4cfec5
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-08-09 12:43:48 +0800

    fix org-capture

---
+++ .doom.d/.doom.d/config.el
@@ -0,0 +1,305 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+
+;; Note to self after cloning this config on to a new system.
+;; - [ ] Replace all the ~/Desktop entries
+;;
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Joel Lee"
+      user-mail-address "lee.yi.jie.joel@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;;(setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-monokai-pro)
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Desktop/Box")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
+
+(setq org-directory "~/.org/"
+      org-ellipsis " ▼ "
+      org-adapt-indentation nil
+      org-habit-show-habits-only-for-today t)
+
+
+(setq search-highlight t
+      search-whitespace-regexp ".*?"
+      isearch-lax-whitespace t
+      isearch-regexp-lax-whitespace nil
+      isearch-lazy-highlight t
+      isearch-lazy-count t
+      lazy-count-prefix-format " (%s/%s) "
+      lazy-count-suffix-format nil
+      isearch-yank-on-move 'shift
+      isearch-allow-scroll 'unlimited)
+
+(setq direnv-always-show-summary nil)
+
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
+
+;;; notmuch
+(use-package! notmuch
+  :commands (notmuch)
+  :init
+  (map! :desc "notmuch" "<f2>" #'notmuch)
+  (map! :map notmuch-search-mode-map
+        :desc "toggle read" "t" #'+notmuch/toggle-read
+        :desc "Reply to thread" "r" #'notmuch-search-reply-to-thread
+        :desc "Reply to thread sender" "R" #'notmuch-search-reply-to-thread-sender
+        :desc "Filter" "/" #'notmuch-search-filter
+        :desc "Archive All" "A" #'+notmuch-archive-all
+        :desc "Delete All" "D" #'+notmuch-delete-all)
+  (map! :map notmuch-show-mode-map
+        :desc "Next link" "<tab>" #'org-next-link
+        :desc "Previous link" "<backtab>" #'org-previous-link
+        :desc "URL at point" "C-<return>" #'browse-url-at-point)
+  (defun +notmuch/toggle-read ()
+    "toggle read status of message"
+    (interactive)
+    (if (member "unread" (notmuch-search-get-tags))
+        (notmuch-search-tag (list "-unread"))
+      (notmuch-search-tag (list "+unread"))))
+  :config
+  (setq message-auto-save-directory "~/.mail/drafts/"
+        message-send-mail-function 'message-send-mail-with-sendmail
+        sendmail-program (executable-find "msmtp")
+        message-sendmail-envelope-from 'header
+        mail-envelope-from 'header
+        mail-specify-envelope-from t
+        message-sendmail-f-is-evil nil
+        message-kill-buffer-on-exit t
+        notmuch-always-prompt-for-sender t
+        notmuch-archive-tags '("-unread")
+        notmuch-crypto-process-mime t
+        notmuch-hello-sections '(notmuch-hello-insert-saved-searches)
+        notmuch-labeler-hide-known-labels t
+        notmuch-search-oldest-first nil
+        notmuch-archive-tags '("-inbox" "-unread")
+        notmuch-message-headers '("To" "Cc" "Subject" "Bcc")
+        notmuch-saved-searches '((:name "inbox" :query "tag:inbox")
+                                 (:name "unread" :query "tag:inbox and tag:unread")
+                                 (:name "to-me" :query "tag:inbox and tag:to-me")
+                                 (:name "personal" :query "tag:inbox and tag:personal")
+                                 (:name "org-roam" :query "tag:inbox and tag:roam")
+                                 (:name "nus" :query "tag:inbox and tag:nus")
+                                 (:name "drafts" :query "tag:draft")))
+
+  (defun +notmuch-archive-all ()
+    "Archive all the emails in the current view."
+    (interactive)
+    (notmuch-search-archive-thread nil (point-min) (point-max)))
+
+
+  (defun +notmuch-delete-all ()
+    "Archive all the emails in the current view.
+Mark them for deletion by cron job."
+    (interactive)
+    (notmuch-search-tag-all '("+deleted"))
+    (+notmuch-archive-all)))
+
+(after! dired
+  (setq dired-listing-switches "-aBhl  --group-directories-first"
+        dired-dwim-target t
+        dired-recursive-copies (quote always)
+        dired-recursive-deletes (quote top)))
+
+(use-package! dired-narrow
+  :commands (dired-narrow-fuzzy)
+  :init
+  (map! :map dired-mode-map
+        :desc "narrow" "/" #'dired-narrow-fuzzy))
+
+(use-package! deadgrep
+  :if (executable-find "rg")
+  :init
+  (map! "M-s" #'deadgrep))
+
+(use-package! smerge-mode
+  :bind (("C-c h s" . joel/hydra-smerge/body))
+  :init
+  (defun joel/enable-smerge-maybe ()
+    "Auto-enable `smerge-mode' when merge conflict is detected."
+    (save-excursion
+      (goto-char (point-min))
+      (when (re-search-forward "^<<<<<<< " nil :noerror)
+        (smerge-mode 1))))
+  (add-hook 'find-file-hook #'joel/enable-smerge-maybe :append)
+  :config
+  (defhydra joel/hydra-smerge (:color pink
+                                        :hint nil
+                                        :pre (smerge-mode 1)
+                                        ;; Disable `smerge-mode' when quitting hydra if
+                                        ;; no merge conflicts remain.
+                                        :post (smerge-auto-leave))
+    "
+   ^Move^       ^Keep^               ^Diff^                 ^Other^
+   ^^-----------^^-------------------^^---------------------^^-------
+   _n_ext       _b_ase               _<_: upper/base        _C_ombine
+   _p_rev       _u_pper           g   _=_: upper/lower       _r_esolve
+   ^^           _l_ower              _>_: base/lower        _k_ill current
+   ^^           _a_ll                _R_efine
+   ^^           _RET_: current       _E_diff
+   "
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("q" nil "cancel" :color blue)))
+
+(use-package! magit
+  :init
+  (map! "s-g" #'magit-status
+        "C-c g" #'magit-status
+        "s-G" #'magit-blame-addition
+        "C-c G" #'magit-blame-addition)
+  :config
+  (transient-append-suffix 'magit-log "a"
+    '("w" "Wip" magit-wip-log-current))
+  (magit-define-popup-switch 'magit-log-popup
+                             ?m "Omit merge commits" "--no-merges")
+  (transient-append-suffix 'magit-log "-A"
+    '("-m" "Omit merge commits" "--no-merges")))
+
+(use-package! git-link
+  :commands
+  (git-link git-link-commit git-link-homepage)
+  :custom
+  (git-link-use-commit t))
+
+(use-package! easy-kill
+  :bind*
+  (([remap kill-ring-save] . easy-kill)))
+
+(use-package! smartparens
+  :init
+  (map! :map smartparens-mode-map
+        "C-M-f" #'sp-forward-sexp
+        "C-M-b" #'sp-backward-sexp
+        "C-M-u" #'sp-backward-up-sexp
+        "C-M-d" #'sp-down-sexp
+        "C-M-p" #'sp-backward-down-sexp
+        "C-M-n" #'sp-up-sexp
+        "C-M-s" #'sp-splice-sexp
+        "C-)" #'sp-forward-slurp-sexp
+        "C-}" #'sp-forward-barf-sexp
+        "C-(" #'sp-backward-slurp-sexp
+        "C-M-)" #'sp-backward-slurp-sexp
+        "C-M-)" #'sp-backward-barf-sexp))
+
+
+
+(after! org
+  (use-package! ol-notmuch
+  :init
+  (map! :map notmuch-show-mode-map "C" #'joel/org-capture-email)
+  (defun joel/org-capture-email ()
+    (interactive)
+    (org-capture nil "e")))
+  (require 'org-habit)
+
+  (with-eval-after-load 'flycheck
+    (flycheck-add-mode 'proselint 'org-mode))
+
+  (map! :leader
+        :prefix "n"
+        "c" #'org-capture)
+  (map! :map org-mode-map
+        "M-n" #'outline-next-visible-heading
+        "M-p" #'outline-previous-visible-heading)
+  (setq org-src-window-setup 'current-window
+        org-return-follows-link t
+        org-babel-load-languages '((emacs-lisp . t)
+                                   (python . t)
+                                   (dot . t)
+                                   (R . t))
+        org-confirm-babel-evaluate nil
+        org-use-speed-commands t
+        org-catch-invisible-edits 'show
+        org-preview-latex-image-directory "/tmp/ltximg/"
+        org-structure-template-alist '(("a" . "export ascii")
+                                       ("c" . "center")
+                                       ("C" . "comment")
+                                       ("e" . "example")
+                                       ("E" . "export")
+                                       ("h" . "export html")
+                                       ("l" . "export latex")
+                                       ("q" . "quote")
+                                       ("s" . "src")
+                                       ("v" . "verse")
+                                       ("el" . "src emacs-lisp")
+                                       ("d" . "definition")
+                                       ("t" . "theorem")))
+;; Here are some additional functions/macros that could help you configure Doom:
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+  (defun joel/org-archive-done-tasks ()
+    "Archive all done tasks."
+    (interactive)
+    (org-map-entries 'org-archive-subtree "/DONE" 'file))
+  (require 'find-lisp)
+  (setq joel/org-agenda-directory (file-truename "~/.org/gtd/"))
+  (setq org-agenda-files
+        (find-lisp-find-files joel/org-agenda-directory "\.org$")))
+
+(setq org-capture-templates
+        `(("i" "inbox" entry (file ,(concat joel/org-agenda-directory "inbox.org"))
+           "* TODO %?")
+          ("e" "email" entry (file+headline ,(concat joel/org-agenda-directory "emails.org") "Emails")
+               "* TODO [#A] Reply: %a :@home:@office:"
+               :immediate-finish t)
+          ("c" "org-protocol-capture" entry (file ,(concat joel/org-agenda-directory "inbox.org"))
+               "* TODO [[%:link][%:description]]\n\n %i"
+               :immediate-finish t)
+          ("w" "Weekly Review" entry (file+olp+datetree ,(concat joel/org-agenda-directory "reviews.org"))
+           (file ,(concat joel/org-agenda-directory "templates/weekly_review.org")))))
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+        (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)")))
